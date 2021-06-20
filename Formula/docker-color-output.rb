9f785a8aeeedb3d24f51d23b9bb076585420fcf7
Sergey Sorokin <s.sorokin@progforce.com>
2021-06-20 16:54:18 +0300

    Add formula

---
+++ Formula/docker-color-output.rb
@@ -0,0 +1,20 @@
+class DockerColorOutput < Formula
+  desc "This package allows to colorize the docker output"
+  homepage "https://github.com/devemio/docker-color-output"
+  url "https://github.com/devemio/docker-color-output/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "f0585ec1ae03608215bcb389eb6ce4bd34f5c4409a190f80b8d59140b353933a"
+  license "MIT"
+
+  depends_on "cmake" => :build
+  depends_on "go" => :build
+
+  def install
+    system "make"
+    mv "bin/dco-darwin-amd64", "docker-color-output"
+    bin.install "docker-color-output"
+  end
+
+  test do
+    system "false"
+  end
+end
