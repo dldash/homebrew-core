6043c0e0fd266dec84c4ab58ba29a066594ee68b
Sergey Sorokin <devemlight@gmail.com>
2024-02-11 03:44:22 +0300

    2.5.1

--- Formula/docker-color-output.rb
+++ Formula/docker-color-output.rb
@@ -1,8 +1,8 @@
 class DockerColorOutput < Formula
   desc "This package allows to colorize the docker output"
   homepage "https://github.com/devemio/docker-color-output"
-  url "https://github.com/devemio/docker-color-output/archive/refs/tags/2.5.0.tar.gz"
-  sha256 "8c96cd5fcb3d9bc3bc5474af4395a986d948a72ae092732e272a6896a988bf70"
+  url "https://github.com/devemio/docker-color-output/archive/refs/tags/2.5.1.tar.gz"
+  sha256 "fbc33a3280c4ac3676a1c1cfa625d8b48928416a88b40d13b72330dc6ee7076c"
   license "MIT"
 
   depends_on "cmake" => :build
---
+++ publish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+formula=./Formula/docker-color-output.rb
+
+echo "👌 Go to Taps" && \
+cd /usr/local/Homebrew/Library/Taps/dldash/homebrew-core && \
+
+echo "👌 Downloading the latest release from GitHub..." && \
+latest_release=$(curl -s https://api.github.com/repos/devemio/docker-color-output/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') && \
+wget -q https://github.com/devemio/docker-color-output/archive/refs/tags/$latest_release.tar.gz && \
+echo "👌 Release [0;36m$latest_release[0m downloaded" && \
+
+sha256=$(shasum -a 256 "$latest_release.tar.gz" | awk '{print $1}') && \
+echo "👌 SHA256 [0;36m$sha256[0m calculated" && \
+
+sed -i '' -E "s/^  url \"(.+)\"/  url \"https:\/\/github.com\/devemio\/docker-color-output\/archive\/refs\/tags\/$latest_release.tar.gz\"/g" $formula && \
+sed -i '' -E "s/^  sha256 \"(.+)\"/  sha256 \"$sha256\"/g" $formula && \
+echo "👌 Formula updated" && \
+
+rm "$latest_release.tar.gz" && \
+echo "👌 Release removed" && \
+
+git add . && git commit -m "$latest_release" && git push && \
+echo "👌 Release published"
