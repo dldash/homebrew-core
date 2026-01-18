2ef4181ae79d1f8efdd7252668911e1b43f2e747
Sergey Sorokin <devemlight@gmail.com>
2026-01-18 18:16:10 +0300

    v3.0.0

--- Formula/docker-color-output.rb
+++ Formula/docker-color-output.rb
@@ -1,8 +1,8 @@
 class DockerColorOutput < Formula
   desc "This package allows to colorize docker output"
   homepage "https://github.com/devemio/docker-color-output"
-  url "https://github.com/devemio/docker-color-output/archive/refs/tags/2.6.1.tar.gz"
-  sha256 "ba4d4935e6745ca8976e48f9c71ccd9978d8cc24a79a9048451df1c0b855f58d"
+  url "https://github.com/devemio/docker-color-output/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "f3cad11d729caebc7b68dd6794be9b3f4fc0cbd4d050548d4558f4cd509f4cc2"
   license "MIT"
 
   depends_on "cmake" => :build
